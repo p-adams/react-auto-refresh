937f4994384157086e2c7df37bf23fc5741486d6
p-adams <ghusiyy@gmail.com>
2021-12-22 23:57:27 -0800

    init handler for post requests

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,10 @@ const server: FastifyInstance = Fastify({});
 server.get("/", async (req, res) => {
   return { msg: "Meow" };
 });
+server.post("/", (req, reply) => {
+  console.log("post");
+  reply.send({ status: 200 });
+});
 
 async function main() {
   try {
