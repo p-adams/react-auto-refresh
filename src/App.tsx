6131ca99f61b1ba2f61406cd13773f8fa825a626
p-adams <ghusiyy@gmail.com>
2021-12-22 23:31:47 -0800

    restructure route components and add react query dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,11 @@
 import { useState } from "react";
 import { Routes, Route, Link, Outlet } from "react-router-dom";
-
+import Layout from "./Layout";
+import Home from "./Home";
+import Results from "./Results";
+import NoMatch from "./NoMatch";
 import "./App.css";
-function Layout() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
 
-      <hr />
-
-      <Outlet />
-    </div>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
-  );
-}
 function App() {
   const [count, setCount] = useState(0);
 
@@ -71,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="results" element={<Results />} />
 
           <Route path="*" element={<NoMatch />} />
         </Route>
---
+++ src/Results.tsx
@@ -0,0 +1,9 @@
+function Results() {
+  return (
+    <div>
+      <h2>Results</h2>
+    </div>
+  );
+}
+
+export default Results;
