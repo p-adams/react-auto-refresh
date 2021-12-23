b575d6a8420304de70871fd00b3efa7f2796c689
p-adams <ghusiyy@gmail.com>
2021-12-23 00:56:43 -0800

    allocate array to store votes votes

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,40 @@
 import Fastify, { FastifyInstance } from "fastify";
 const server: FastifyInstance = Fastify({});
+const votes: Array<FormData> = [];
+
+/**
+ * sample request:
+ * {
+	"data": {
+		"email": "roger@acme.com",
+		"candidate": {
+			"id": "_idx45xfE0",
+			"full_name": "Fred Flintstone"
+		}
+	}
+}
+ */
+interface FormData {
+  email: string;
+  candidate: {
+    id: string;
+    full_name: string;
+  };
+}
+
+interface FormType {
+  data: FormData;
+}
 server.get("/", async (req, res) => {
   return { msg: "Meow" };
 });
-server.post("/", (req, reply) => {
-  console.log("post");
+server.post<{ Body: FormType }>("/", (req, reply) => {
+  const { data } = req.body;
+  const existingVote = votes.find((vote) => vote.email === data.email);
+  if (!existingVote) {
+    votes.push(data);
+  }
+
   reply.send({ status: 200 });
 });
 
