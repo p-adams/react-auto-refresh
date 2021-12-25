6ebca8b64ed57ace8509181dc414d5798ce4e9e3
p-adams <ghusiyy@gmail.com>
2021-12-24 20:06:03 -0800

    wire up candidate list to handler

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,28 +3,35 @@ function Home() {
   const [candidates, setCandidates] = useState<
     Array<{ id?: string; name: string }>
   >([]);
+  const [currentCandidate, setCurrentCandidate] =
+    useState<{ id?: string; name: string }>();
   useEffect(() => {
     fetch("/api/candidates")
       .then((res) => res.json())
       .then((res) => setCandidates(res));
   }, []);
   return (
-    <div>
-      <h2>Cast Your Vote for Mayor of Bedrock</h2>
-      <form>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <form style={{ margin: "0 auto" }}>
+        <h2>Cast Your Vote for The Next Mayor of Bedrock</h2>
         <input type="email" />
-        <ul>
+        <ul style={{ marginTop: "10px" }}>
           {candidates.map((candidate) => {
             return (
-              <li>
-                <label>
-                  <input type="radio" />
+              <li key={candidate.id}>
+                <label onClick={() => setCurrentCandidate(candidate)}>
+                  <input
+                    type="radio"
+                    checked={candidate.id === currentCandidate?.id}
+                    readOnly
+                  />
                   {candidate.name}
                 </label>
               </li>
             );
           })}
         </ul>
+        <button type="submit">Vote</button>
       </form>
     </div>
   );
