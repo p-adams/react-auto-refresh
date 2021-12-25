f61c157fa29229629cd10187e932eca0080b9ceb
p-adams <ghusiyy@gmail.com>
2021-12-24 19:43:10 -0800

    render candidates in UI

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 function Home() {
+  const [candidates, setCandidates] = useState<
+    Array<{ id?: string; name: string }>
+  >([]);
+  useEffect(() => {
+    fetch("/api/candidates")
+      .then((res) => res.json())
+      .then((res) => setCandidates(res));
+  }, []);
   return (
     <div>
       <h2>Cast Your Vote for Mayor of Bedrock</h2>
       <form>
         <input type="email" />
-        {/** TODO render candidates list */}
+        <ul>
+          {candidates.map((candidate) => {
+            return (
+              <li>
+                <label>
+                  <input type="radio" />
+                  {candidate.name}
+                </label>
+              </li>
+            );
+          })}
+        </ul>
       </form>
     </div>
   );
